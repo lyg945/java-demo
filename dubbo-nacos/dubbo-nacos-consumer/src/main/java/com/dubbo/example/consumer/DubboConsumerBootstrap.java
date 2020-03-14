71d529174790c2e69267a8ee03ff63f5fd04ca5e
刘永刚 <liuyg945@gmail.com>
2020-03-14 21:17:28 +0800

    dubbo 注册中心改nacos

---
+++ dubbo-nacos/dubbo-nacos-consumer/src/main/java/com/dubbo/example/consumer/DubboConsumerBootstrap.java
@@ -0,0 +1,28 @@
+package com.dubbo.example.consumer;
+import com.dubbo.example.DemoService;
+import org.apache.dubbo.config.annotation.Reference;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class DubboConsumerBootstrap {
+    private final Logger logger = LoggerFactory.getLogger(getClass());
+
+    @Reference
+    private DemoService demoService;
+
+    public static void main(String[] args) {
+        SpringApplication.run(DubboConsumerBootstrap.class);
+    }
+
+    @Bean
+    public ApplicationRunner runner() {
+        return args -> {
+            logger.info(demoService.sayHello("*************hello-dubbo-for-nacos*************"));
+        };
+    }
+}
--- issues/link.md
+++ issues/link.md
@@ -1,2 +1,3 @@
 dubbo: https://github.com/apache/dubbo
 dubbo-spring-boot-project: https://github.com/apache/dubbo-spring-boot-project
+nacos:  https://nacos.io/en-us/docs/use-nacos-with-dubbo.html
