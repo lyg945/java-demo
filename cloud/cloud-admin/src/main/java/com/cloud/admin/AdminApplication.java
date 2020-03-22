448883e7a8c33249f9fedd8a1bb708c549c7b524
刘永刚 <liuyg945@gmail.com>
2020-03-22 12:25:26 +0800

    添加注册中心，配置中心nacos

--- cloud/cloud-admin/src/main/java/com/cloud/admin/AdminApplication.java
+++ cloud/cloud-admin/src/main/java/com/cloud/admin/AdminApplication.java
@@ -3,11 +3,15 @@ package com.cloud.admin;
 import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
 @Configuration
 @EnableAdminServer
+@EnableDiscoveryClient
+@RefreshScope
 public class AdminApplication {
 
     public static void main(String[] args) {
